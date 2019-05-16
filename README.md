97ab0530aafadfd2a4ca9800d914bf44ac5a2400
Juncheol Cho <zironycho@gmail.com>
2019-05-16 11:50:00 +0900

    add silence option

--- README.md
+++ README.md
@@ -7,12 +7,29 @@ make run
 ```
 
 ## api
-```
+```bash
+# simple ogg
 curl -X POST localhost:32769/wav_to_ogg\
   -o out.ogg \
   -F "wav=@test/test.wav"
 
+# full option for ogg
+curl -X POST localhost:32769/wav_to_ogg\
+  -o out.ogg \
+  -F "wav=@test/test.wav" \
+  -F "silence=5"
+
+# simple mp3
 curl -X POST localhost:32769/wav_to_mp3\
   -o out.mp3 \
   -F "wav=@test/test.wav"
+
+# full option for mp3
+curl -X POST localhost:32769/wav_to_mp3\
+  -o out.mp3 \
+  -F "wav=@test/test.wav" \
+  -F "silence=5" \
+  -F "bitrate=320k" \
+  -F "sampling_rate=44100"
+
 ```
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,7 @@
 from app.app import app
 from unittest import TestCase
+from io import BytesIO
+from mutagen.mp3 import MP3
 
 
 class Test(TestCase):
@@ -33,3 +35,17 @@ class Test(TestCase):
 
             self.assertEqual(200, r.status_code)
             self.assertEqual(b'OggS', r.data[:4])
+
+    def test_wav_to_mp3_with_silence(self):
+        cli = app.test_client()
+        with open('./tests/assets/test.wav', 'rb') as f:
+            r = cli.post('/wav_to_mp3', content_type='multipart/form-data', data={
+                'wav': f,
+                'silence': '5',
+            }, buffered=True)
+
+            self.assertEqual(200, r.status_code)
+            self.assertEqual(b'ID3', r.data[:3])
+            audio= BytesIO(r.data)
+            duration = MP3(audio).info.length
+            self.assertTrue(5 < duration)
